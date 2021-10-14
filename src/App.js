aee7ab4b852e26dc8ced0a8e3d9eedbf25e99f07
jslee <jslee@kb-sys.co.kr>
2021-10-14 15:47:49 +0900

    img image 주의

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Food = ({name, picture}) => {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} style={{width:'100px'}} />
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    name: "kimchi",
+    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    img: "http://www.samda.com/shopimages/samdacom1/0200020000012.jpg?1634119503",
+  },
+  {
+    name: "bibimbap",
+    image: "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjky/MDAxNDkzMDIzMjY5MjY5.u5q3w1Cl1wjxJoRzSpczd_VT2rScn_zaDV5LfKVMa6Mg.Tz5B-6i_SYH4yydceeZT6bNwDs-P5srXOE2uCDQ_Disg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800",
+  },
+  {
+    name: "Kimbap",
+    img: "https://dimg.donga.com/ugc/CDB/WOMAN/Article/5e/dd/c1/1a/5eddc11a003fd2738de6.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodLike.map((dish) => (
+        <Food name={dish.name} picture={dish.img} />
+      ))}
     </div>
   );
 }
