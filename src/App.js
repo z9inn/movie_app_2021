27d0473fe7aee4efe7273df9a8ccc75be0cbd7f4
jeomrye <sscc57017@gmail.com>
2021-10-14 23:26:10 +0900

    isLoading true -> false 변경함

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ class App extends React.Component{
     isLoading: true, // this.state
     movies: [],
   };
-
-  getMovies = async () => { // async는 자바스크립트에게 getMovies() 함수는 시간이 필요하고
-                            // async는 JS에게 getMovies() 함수가 비동기라고 말해주는 것. JS에게 getMovies() 함수는 비동기라서 기다려야 해라고 말한거라고 생각하면 됨
-    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json'); // await.get()의 실행을 기다려달라고 말해주는 것
+  // async는 자바스크립트에게 getMovies() 함수는 시간이 필요하고
+  // async는 JS에게 getMovies() 함수가 비동기라고 말해주는 것. JS에게 getMovies() 함수는 비동기라서 기다려야 해라고 말한거라고 생각하면 됨
+  getMovies = async () => { 
+    const { 
+      data: {
+        data: { movies }, // 여기 movies는 axios.get()의 결과를 담을 변수이고, 위 state에 있는 movies는 변수가 아닌 state다. 엄연히 다른것이니 혼동하지 말자
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json'); // await.get()의 실행을 기다려달라고 말해주는 것
                                                                                 // await은 JS에게 getMovies() 함수 내부의 axios.get()의 실행 완료를 기다렸다가 끝나면 계속 진행하라고 말한 것
     // 여기서 중요한 내용은 API에서 데이터를 받아오는 axios.get()을 실행하려면 시간이 필요하고, 그 사실을 JS에게 알려야만 데이터를 잡을수 있으므로 async, await을 사용 했다는 것
     // async와 await은 짝꿍이다 그래서 둘은 동시에 사용해야만 한다!!!!
-    console.log(movies); // console
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount() { // App 컴포넌트가 그려지면(render() 함수가 실행되면) 호출되는 생명주기 함수는 componentDidMount() 함수다
