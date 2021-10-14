5d0facab61e83bcfc9596ef6bd48221190d8a928
jslee <jslee@kb-sys.co.kr>
2021-10-14 17:30:19 +0900

    prop-type. rating string -> number

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Food = ({name, picture}) => {
+const Food = ({name, picture, rating}) => {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating} / 5</h4>
       <img src={picture} style={{ width: '100px', height: '100px' }} alt={name}/>
     </div>
   );
@@ -22,21 +24,25 @@ const foodLike = [
     id: 1,
     name: "kimchi",
     img: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5,
   },
   {
     id: 2,
     name: "Samgyeopsal",
     img: "http://www.samda.com/shopimages/samdacom1/0200020000012.jpg?1634119503",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "bibimbap",
     img: "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjky/MDAxNDkzMDIzMjY5MjY5.u5q3w1Cl1wjxJoRzSpczd_VT2rScn_zaDV5LfKVMa6Mg.Tz5B-6i_SYH4yydceeZT6bNwDs-P5srXOE2uCDQ_Disg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800",
+    rating: 4.4,
   },
   {
     id: 4,
     name: "Kimbap",
     img: "https://dimg.donga.com/ugc/CDB/WOMAN/Article/5e/dd/c1/1a/5eddc11a003fd2738de6.jpg",
+    rating: 4.3,
   },
 ];
 
@@ -48,12 +54,18 @@ function App() {
   return (
     <div className="App">
       {foodLike.map(dish => ( //Arrow Function은 함수명과 리턴값이 없어도 사용할 수 있다. 굳이 function 키워드가 없어도 되고 return을 사용하지 않아도 식을 계산한 값이 자동으로 변환된다
-        <Food key={dish.id} name={dish.name} picture={dish.img} /> //key props는 리액트 내부에서 사용되는 특수한 props이기 때문에 내가 직접 만들 props와 다르게 Food 컴포넌트에 직접 전달되지 않는다
+        <Food key={dish.id} name={dish.name} picture={dish.img} rating={dish.rating} /> //key props는 리액트 내부에서 사용되는 특수한 props이기 때문에 내가 직접 만들 props와 다르게 Food 컴포넌트에 직접 전달되지 않는다
       )
     )
   }
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired, //isRequired: 필요하다
+  rating: PropTypes.string.isRequired, //string + isRequired는 문자열 자료형이 필요하다. 라는 뜻 인데 rating은 숫자로 이루어져 있는 데이터이니 string이 아닌 number 타입이어야 맞다
+};
 
 export default App;
