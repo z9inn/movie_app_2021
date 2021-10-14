8b9f58ac6f85a2fc0168e8ec2f9585df4cb874a9
jeomrye <sscc57017@gmail.com>
2021-10-14 23:13:26 +0900

    영화 api를 가져왔고 async와 await을 사용했다

--- src/App.js
+++ src/App.js
@@ -8,9 +8,18 @@ class App extends React.Component{
     movies: [],
   };
 
+  getMovies = async () => { // async는 자바스크립트에게 getMovies() 함수는 시간이 필요하고
+                            // async는 JS에게 getMovies() 함수가 비동기라고 말해주는 것. JS에게 getMovies() 함수는 비동기라서 기다려야 해라고 말한거라고 생각하면 됨
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json'); // await.get()의 실행을 기다려달라고 말해주는 것
+                                                                                // await은 JS에게 getMovies() 함수 내부의 axios.get()의 실행 완료를 기다렸다가 끝나면 계속 진행하라고 말한 것
+    // 여기서 중요한 내용은 API에서 데이터를 받아오는 axios.get()을 실행하려면 시간이 필요하고, 그 사실을 JS에게 알려야만 데이터를 잡을수 있으므로 async, await을 사용 했다는 것
+    // async와 await은 짝꿍이다 그래서 둘은 동시에 사용해야만 한다!!!!
+    console.log(movies); // console
+  }
+
   componentDidMount() { // App 컴포넌트가 그려지면(render() 함수가 실행되면) 호출되는 생명주기 함수는 componentDidMount() 함수다
     // 영화 데이터 로딩! (영화 데이터 로딩이 완료되면 이란뜻)
-    axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.getMovies();
   };
 
   render() {
