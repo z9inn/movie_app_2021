3b1ddb590446d0604650b391481c2a75839f882a
jslee <jslee@kb-sys.co.kr>
2021-10-15 12:28:53 +0900

    완성 전 배포해보기

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { HashRouter, Route } from "react-router-dom";
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" component={Home} exact={true} /> {/* exact props는 Route 컴포넌트가 path props가 정확하게 일치하는 URL에만 반응한다 */}
       <Route path="/about" component={About} />
     </HashRouter>
