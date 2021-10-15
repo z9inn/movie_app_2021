657028d7310d02ecb2c865394bc106069ef9a500
jslee <jslee@kb-sys.co.kr>
2021-10-15 17:56:00 +0900

    터미널에서만 커밋 푸시 되는지 마지막으로 확인

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -51,6 +51,7 @@ class Home extends React.Component {
                 genres={movie.genres}
               />
             ))}
+              <h1 style={{ color: 'red'}}>확인</h1>
           </div>
         )}
       </section>
