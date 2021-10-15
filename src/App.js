c006a31b2ad5d3f437adab7f4a1da69a5f07f776
jslee <jslee@kb-sys.co.kr>
2021-10-15 15:23:58 +0900

    왜 안되냐?

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Route } from "react-router-dom";
 import About from './routes/About';
 import Home from './routes/Home';
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Route path="/" component={Home} exact={true} /> {/* exact props는 Route 컴포넌트가 path props가 정확하게 일치하는 URL에만 반응한다 */}
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import { Link } from 'react-router-dom';
 
 function Movie({ title, year, summary, poster, genres }) {
     return (
       <div className="movie">
+        <Link
+          to={{
+            pathname: '/movie-detail',
+            state: { year, title, summary, poster, genres },
+          }}
+        >
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
@@ -14,8 +21,9 @@ function Movie({ title, year, summary, poster, genres }) {
                         return <li key={index} className="movie__genre">{genre}</li>;
                     })}                
                 </ul>
-        <p className="movie__summary">{summary.slice(0,180)}....</p>
-        </div>
+            <p className="movie__summary">{summary.slice(0,180)}....</p>
+          </div>
+        </Link>
       </div>
     );
 }
