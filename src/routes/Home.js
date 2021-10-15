14b22bcfad5d7e561ed77fc5eb1b8dcd4a419d48
jslee <jslee@kb-sys.co.kr>
2021-10-15 17:33:47 +0900

    최종

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -51,7 +51,6 @@ class Home extends React.Component {
                 genres={movie.genres}
               />
             ))}
-              <h1 style={{color:"red"}}>Shit!</h1>
           </div>
         )}
       </section>
